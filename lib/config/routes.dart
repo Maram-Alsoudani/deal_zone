0e85c4e863c062184b7d8cfede323acb0e4218f4
Maram <maram.alsoudani@gmail.com>
2024-12-29 22:29:23 +0200

    Finished layout of register screen.

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,10 +1,13 @@
 import 'package:deal_zone/features/login/presentation/pages/login_screen.dart';
+import 'package:deal_zone/features/register/presentation/pages/register_screen.dart';
 import 'package:go_router/go_router.dart';
 
 import '../features/splash/splash_screen.dart';
 class AppRoutes {
   static const String splashScreen = '/splash';
   static const String loginScreen = '/login';
+  static const String registerScreen = '/register';
+
 
   static final GoRouter router = GoRouter(
     initialLocation: splashScreen,
@@ -16,6 +19,10 @@ class AppRoutes {
       GoRoute(
         path: loginScreen,
         builder: (context, state) => LoginScreen(),
+      ),
+      GoRoute(
+        path: registerScreen,
+        builder: (context, state) => RegisterScreen(),
       ),
     ],
   );
